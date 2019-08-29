e7ecfcf0789f6691343e8e970eff79fa64d3a77b
biuld <xboltk@gmail.com>
2019-08-29 11:23:44 +0800

    调整包结构，添加拓扑排序测试

--- src/test/java/com/wemeet/AppTest.java
+++ src/test/java/com/wemeet/AppTest.java
@@ -4,7 +4,9 @@ import static org.junit.Assert.assertEquals;
 
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.builder.GraphBuilder;
 import org.jgrapht.graph.builder.GraphTypeBuilder;
+import org.jgrapht.traverse.TopologicalOrderIterator;
 import org.jgrapht.util.SupplierUtil;
 import org.junit.Test;
 
@@ -41,5 +43,39 @@ public class AppTest
         assertEquals(2, graph.outDegreeOf(v0));
         assertEquals(1, graph.outDegreeOf(v1));
         assertEquals(0, graph.outDegreeOf(v2));
+    }
+
+    @Test
+    public void topologicalSortingTest() {
+        //有向无环图
+        Graph<String, DefaultEdge> directedAcyclicGraph = GraphTypeBuilder
+                .directed()
+                .allowingMultipleEdges(false)
+                .allowingSelfLoops(false)
+                .vertexClass(String.class) //点类型
+                .edgeClass(DefaultEdge.class) //边类型
+                .buildGraph();
+
+        Graph<String, DefaultEdge> graph = new GraphBuilder<>(directedAcyclicGraph)
+                .addEdgeChain("程序设计基础", "Java面向对象编程", "数据结构", "计算机网络")
+                .addEdgeChain("程序设计基础", "Java面向对象编程", "计算机组成原理", "操作系统")
+                .addEdgeChain("程序设计基础", "Java面向对象编程", "数据结构", "操作系统")
+                .addEdgeChain("程序设计基础", "Java面向对象编程", "计算机组成原理", "计算机网络")
+                .build();
+
+        TopologicalOrderIterator<String, DefaultEdge> topologicalOrderIterator = new TopologicalOrderIterator<>(graph);
+
+        StringBuilder builder = new StringBuilder();
+
+        try {
+            topologicalOrderIterator.forEachRemaining(elem -> builder.append(elem).append(" -> "));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        builder.append("END");
+        System.out.println(builder.toString());
+
+//        graph.edgeSet().forEach(System.out::println);
     }
 }
